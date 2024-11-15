5806cef6aaa30542a4e8bfa83953fc70794598e1
Christian Liebel <christian.liebel@thinktecture.com>
2024-11-15 10:27:47 +0100

    Lab #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from './nav/nav.component';
+import {CreateMLCEngine, MLCEngine} from '@mlc-ai/web-llm';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,24 @@ import {NavComponent} from './nav/nav.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'genai-app';
+
+  protected readonly progress = signal(0);
+  protected readonly ready = signal(false);
+  protected engine?: MLCEngine;
+
+  async ngOnInit() {
+    const model = 'Llama-3.2-3B-Instruct-q4f16_1-MLC';
+    this.engine = await CreateMLCEngine(model, {
+      initProgressCallback: ({ progress }) => {
+        this.progress.set(progress);
+      }
+    });
+    this.ready.set(true);
+  }
+
+  runPrompt(value: string) {
+
+  }
 }
